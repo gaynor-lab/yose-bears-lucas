2f60f06fbeb6d3e6275e6e4669eb470407d5e7ee
Lucas Braun <lfbraun10@gmail.com>
2024-11-15 18:47:29 -0800

    Practicing Time Series Regressions

---
+++ TimeSeriesRegressionPractice.R
@@ -0,0 +1,22 @@
+#A lesson in Time Series
+#Based on Forecasting: Principles and Practice
+#Merging datasets and running a few basic regressions with Property damage cost as the 
+#forecasted variable
+
+#Replace property type IDs with their corresponding strings
+
+incidentData <- incidentData %>%
+  mutate(PropertyTypeID = case_when(
+    PropertyTypeID == 11 ~ "Tent",
+    PropertyTypeID == 12 ~ "Pack",
+    PropertyTypeID == 14 ~ "Ice-chest",
+    PropertyTypeID == 13 ~ "Foodsack",
+    PropertyTypeID == 15 ~ "Building",
+    PropertyTypeID == 16 ~ "Towed-unit",
+    PropertyTypeID == 17 ~ "Motor Vehicle",
+    PropertyTypeID == 18 ~ "Food",
+    PropertyTypeID == 19 ~ "Other",
+    PropertyTypeID == 20 ~ "No Damage",
+    PropertyTypeID == 21 ~ "Portable Food Storage Containers",
+    TRUE ~ NA_character_  # To handle any other cases
+  ))
